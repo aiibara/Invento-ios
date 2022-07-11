5dad0275bb29f2c67fac5a9b8fc20161a459eb1b
aiibara <widya.limarto@gmail.com>
2022-07-11 14:17:42 +0700

    first-commit

---
+++ Invento/InventoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoApp.swift
+//  Invento
+//
+//  Created by Widya Limarto on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct InventoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
